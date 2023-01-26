8130b6340d1fd4c2befaa1f6ed53e722da77f5b6
Derek Berggren <dberggren@heartagainstudios.com>
2023-01-25 23:03:38 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Derek Berggren on 2023.01.25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
