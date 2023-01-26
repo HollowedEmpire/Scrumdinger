16f6bf3a1779780207d0a964973cabd4651bb077
Derek Berggren <dberggren@heartagainstudios.com>
2023-01-26 17:41:51 -0600

    Navigation and modal presentation - Creating a navigation hierarchy: Section 3: Add visual components to the detail view

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -13,7 +13,7 @@ struct ScrumsView: View {
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                NavigationLink(destination: DetailView(scum: scrum)) {
+                NavigationLink(destination: DetailView(scrum: scrum)) {
                     CardView(scrum: scrum)
                 }
                 .listRowBackground(scrum.theme.mainColor)
